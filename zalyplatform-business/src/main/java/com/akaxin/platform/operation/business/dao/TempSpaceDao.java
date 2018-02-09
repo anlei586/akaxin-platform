b0d03dcd2e9be0723447e75a2cde6a45b3309758
SAM2O2O <an.guoyue254@gmail.com>
2018-02-09 21:21:01 +0800

    增加download upload 日志

--- zalyplatform-business/src/main/java/com/akaxin/platform/operation/business/dao/TempSpaceDao.java
+++ zalyplatform-business/src/main/java/com/akaxin/platform/operation/business/dao/TempSpaceDao.java
@@ -1,10 +1,14 @@
 package com.akaxin.platform.operation.business.dao;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.akaxin.common.utils.StringHelper;
 import com.akaxin.platform.storage.api.ITempSpaceDao;
 import com.akaxin.platform.storage.service.TempSpaceDaoService;
 
 public class TempSpaceDao {
-
+	private static final Logger logger = LoggerFactory.getLogger(TempSpaceDao.class);
 	private static TempSpaceDao instance = new TempSpaceDao();
 	private ITempSpaceDao tempSpaceDao = new TempSpaceDaoService();
 
@@ -17,17 +21,22 @@ public class TempSpaceDao {
 		try {
 			result = tempSpaceDao.setStringValue(key, value, expireTime);
 		} catch (Exception e) {
-			e.printStackTrace();
+			logger.info(StringHelper.format("apply temp space key={} value={} expireTime={}", key, value, expireTime),
+					e);
 		}
+		logger.info(StringHelper.format("apply temp space key={} value={} expireTime={} result={}", key, value,
+				expireTime, result));
 		return result;
 	}
 
 	public String getTempValue(String keyName) {
+		String value = null;
 		try {
-			return tempSpaceDao.getStringValue(keyName);
+			value = tempSpaceDao.getStringValue(keyName);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return null;
+		logger.info(StringHelper.format("get temp space key={} value={}", keyName, value));
+		return value;
 	}
 }
--- zalyplatform-business/src/main/java/com/akaxin/platform/operation/business/handler/ApiTempHandler.java
+++ zalyplatform-business/src/main/java/com/akaxin/platform/operation/business/handler/ApiTempHandler.java
@@ -11,11 +11,11 @@ import com.akaxin.common.command.Command;
 import com.akaxin.common.command.CommandResponse;
 import com.akaxin.common.constant.CommandConst;
 import com.akaxin.common.constant.ErrorCode;
+import com.akaxin.common.constant.ErrorCode2;
 import com.akaxin.platform.operation.business.dao.TempSpaceDao;
 import com.akaxin.proto.platform.ApiTempDownloadProto;
 import com.akaxin.proto.platform.ApiTempUploadProto;
 import com.google.protobuf.ByteString;
-import com.google.protobuf.InvalidProtocolBufferException;
 
 /**
  * 
@@ -27,7 +27,6 @@ public class ApiTempHandler extends AbstractApiHandler<Command> {
 	private static final Logger logger = LoggerFactory.getLogger(ApiTempHandler.class);
 
 	public boolean upload(Command command) {
-		logger.info("-----api.tmp.upload-----");
 		CommandResponse commandResponse = new CommandResponse().setVersion(CommandConst.PROTOCOL_VERSION)
 				.setAction(CommandConst.ACTION_RES);
 		String errorCode = ErrorCode.ERROR;
@@ -38,6 +37,8 @@ public class ApiTempHandler extends AbstractApiHandler<Command> {
 			if (StringUtils.isEmpty(key)) {
 				key = UUID.randomUUID().toString();
 			}
+			logger.info("api.tmp.upload key={} request={}", key, request.toString());
+
 			String value = Base64.getEncoder().encodeToString(request.getContent().toByteArray());
 			int expireTime = 10 * 60;// 默认10分钟
 			if (TempSpaceDao.getInstance().applyTempSpace(key, value, expireTime)) {
@@ -46,23 +47,24 @@ public class ApiTempHandler extends AbstractApiHandler<Command> {
 				commandResponse.setParams(response.toByteArray());
 				errorCode = ErrorCode.SUCCESS;
 			}
-		} catch (InvalidProtocolBufferException e) {
+		} catch (Exception e) {
 			commandResponse.setErrInfo("apply temp space error");
 			logger.error("upload temp space error.", e);
 		}
+		logger.info("api.tmp.upload result={}", errorCode.toString());
 		command.setResponse(commandResponse.setErrCode(errorCode));
 		return false;
 	}
 
 	public boolean download(Command command) {
-		logger.info("------api.tmp.download-----");
 		CommandResponse commandResponse = new CommandResponse().setVersion(CommandConst.PROTOCOL_VERSION)
 				.setAction(CommandConst.ACTION_RES);
-		String errorCode = ErrorCode.ERROR;
+		ErrorCode2 errorCode = ErrorCode2.ERROR;
 		try {
 			ApiTempDownloadProto.ApiTempDownloadRequest request = ApiTempDownloadProto.ApiTempDownloadRequest
 					.parseFrom(command.getParams());
 			String keyName = request.getName();
+			logger.info("api.tmp.download keyName={} request={}", keyName, request.toString());
 
 			if (keyName != null && keyName.length() > 5) {
 				String contentString = TempSpaceDao.getInstance().getTempValue(keyName);
@@ -71,15 +73,18 @@ public class ApiTempHandler extends AbstractApiHandler<Command> {
 					ApiTempDownloadProto.ApiTempDownloadResponse response = ApiTempDownloadProto.ApiTempDownloadResponse
 							.newBuilder().setContent(ByteString.copyFrom(contentBytes)).build();
 					commandResponse.setParams(response.toByteArray());
-					errorCode = ErrorCode.SUCCESS;
+					errorCode = ErrorCode2.SUCCESS;
 				}
+			}else {
+				errorCode = ErrorCode2.ERROR_PARAMETER;
 			}
 
-		} catch (InvalidProtocolBufferException e) {
+		} catch (Exception e) {
 			commandResponse.setErrInfo("download from temp space error");
 			logger.error("download from temp space error.", e);
 		}
-		command.setResponse(commandResponse.setErrCode(errorCode));
+		logger.info("api.tmp.download result={}", errorCode.toString());
+		command.setResponse(commandResponse.setErrCode2(errorCode));
 		return false;
 	}
 
