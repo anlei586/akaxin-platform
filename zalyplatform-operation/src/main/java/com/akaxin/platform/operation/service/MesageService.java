204104d2f6fca6ab5f899069726ea8b8a374e751
SAM2O2O <an.guoyue254@gmail.com>
2018-05-30 17:44:04 +0800

    释放push发送的接口

--- zalyplatform-operation/src/main/java/com/akaxin/platform/operation/service/MesageService.java
+++ zalyplatform-operation/src/main/java/com/akaxin/platform/operation/service/MesageService.java
@@ -40,7 +40,7 @@ public class MesageService implements IMessage {
 			String action = command.getAction();
 			// 过滤一些不需要session验证的action
 			if ("api.platform.login".equals(action) || "api.platform.registerByPhone".equals(action)
-					|| "api.push.notification".equals(action) || "api.phone.login".equals(action)
+					|| action.startsWith("api.push.notification") || "api.phone.login".equals(action)
 					|| "api.phone.verifyCode".equals(action) || "api.temp.download".equals(action)
 					|| "api.temp.upload".equals(action) || "api.phone.confirmToken".endsWith(action)) {
 				response = ApiOperateExecutor.getExecutor().execute(command.getService(), command);
